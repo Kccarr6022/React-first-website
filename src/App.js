5a47e768362eb0c2ad5839c98b54320d656d7da4
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-10-02 01:06:35 -0400

    Routes and components added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import './App.scss'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
+import Resume from './components/Resume'
+import Projects from './components/Projects'
+import Blog from './components/Blog'
 import Contact from './components/Contact'
+import About from './components/About'
 
 
 function App() {
@@ -12,8 +16,20 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
         </Route>
+        <Route path="/resume" element={<Layout />}>
+          <Route index element={<Resume />} />
+        </Route>
+        <Route path="/projects" element={<Layout />}>
+          <Route index element={<Projects />} />
+        </Route>
+        <Route path="/blog" element={<Layout />}>
+          <Route index element={<Blog />} />
+        </Route>
         <Route path="/contact" element={<Layout />}>
           <Route index element={<Contact />} />
+        </Route>
+        <Route path="/about" element={<Layout />}>
+          <Route index element={<About />} />
         </Route>
       </Routes>
     </>
