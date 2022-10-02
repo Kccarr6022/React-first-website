b72a541fd048fbcbf5af3e8e2bf18f10a7a03152
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-10-02 00:29:42 -0400

    Fixed words per line

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,31 +7,13 @@ import './index.scss'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const nameArray = [' ', 'K', 'a', 'd', 'e', 'n', ' ', 'C', 'a', 'r', 'r']
+  const nameArray = ['K', 'a', 'd', 'e', 'n', ' ', 'C', 'a', 'r', 'r']
   const jobArray = [
-    'S',
-    'o',
-    'f',
-    't',
-    'w',
-    'a',
-    'r',
-    'e',
-    ' ',
-    'E',
-    'n',
-    'g',
-    'i',
-    'n',
-    'e',
-    'e',
-    'r',
-    'i',
-    'n',
-    'g',
+    ['S', 'o', 'f', 't', 'w', 'a', 'r', 'e'],
+    ['E', 'n', 'g', 'i', 'n', 'e', 'e', 'r', 'i', 'n', 'g'],
   ]
 
-  const position = [' ', 'S', 't', 'u', 'd', 'e', 'n', 't', '.']
+  const position = ['S', 't', 'u', 'd', 'e', 'n', 't', '.']
 
   useEffect(() => {
     setTimeout(() => {
@@ -46,9 +28,10 @@ const Home = () => {
           <h1>
             <span className={letterClass}>H</span>
             <span className={`${letterClass} _12`}>I,</span>
-            <br />
+            <span> </span>
             <span className={`${letterClass} _13`}>I</span>
             <span className={`${letterClass} _14`}>'m</span>
+            <br></br>
             <AnimatedLetters
               letterClass={letterClass}
               strArray={nameArray}
@@ -59,13 +42,20 @@ const Home = () => {
             <br />
             <AnimatedLetters
               letterClass={letterClass}
-              strArray={jobArray}
+              strArray={jobArray[0]}
               idx={28}
             />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray[1]}
+              idx={36}
+            />
+            <br />
             <AnimatedLetters
               letterClass={letterClass}
               strArray={position}
-              idx={48}
+              idx={47}
             />
           </h1>
           <h2> Frontend Developer/Designer, Devops & Backend </h2>
