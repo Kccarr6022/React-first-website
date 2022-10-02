6cfef81120fdf49cf87d43e7cb7bf01d48ed4c44
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-10-01 23:38:13 -0400

    Working text effect

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import Logo from '../../assets/images/logo.png'
-import React, { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
@@ -30,52 +31,50 @@ const Home = () => {
     'g',
   ]
 
-  const position = [
-    ' ',
-    'S',
-    't',
-    'u',
-    'd',
-    'e',
-    'n',
-    't',
-    '.',
-  ]
+  const position = [' ', 'S', 't', 'u', 'd', 'e', 'n', 't', '.']
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 5500)
+  }, [letterClass])
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>I,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <span className={`${letterClass} _26`}></span>
-          <span className={`${letterClass} _27`}>a</span>
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={28}
-          />
-          < AnimatedLetters
-            letterClass={letterClass}
-            strArray={position}
-            idx={48}
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>I,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
             />
-        </h1>
-        <h2> Frontend Developer/Designer, Devops & Backend </h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+            <span className={`${letterClass} _26`}></span>
+            <span className={`${letterClass} _27`}>a</span>
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={28}
+            />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={position}
+              idx={48}
+            />
+          </h1>
+          <h2> Frontend Developer/Designer, Devops & Backend </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
