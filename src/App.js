35737a2d5a82608fca0ae285d79fd7099ff1a996
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-10-02 13:39:03 -0400

    Routes adjusted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.scss'
 import { Routes, Route } from 'react-router-dom'
+
+// components
 import Layout from './components/Layout'
 import Home from './components/Home'
 import Resume from './components/Resume'
@@ -8,28 +10,17 @@ import Blog from './components/Blog'
 import Contact from './components/Contact'
 import About from './components/About'
 
-
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-        </Route>
-        <Route path="/resume" element={<Layout />}>
-          <Route index element={<Resume />} />
-        </Route>
-        <Route path="/projects" element={<Layout />}>
-          <Route index element={<Projects />} />
-        </Route>
-        <Route path="/blog" element={<Layout />}>
-          <Route index element={<Blog />} />
-        </Route>
-        <Route path="/contact" element={<Layout />}>
-          <Route index element={<Contact />} />
-        </Route>
-        <Route path="/about" element={<Layout />}>
-          <Route index element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
         </Route>
       </Routes>
     </>
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -8,56 +8,83 @@ import {
   faProjectDiagram,
   faFile,
   faPhone,
-  faBlog
+  faBlog,
 } from '@fortawesome/free-solid-svg-icons' //import the icons
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
   <div className="nav-bar">
-    <Link className="logo" to="/">
+    <Link className="logo" to="/home">
       <img src={Logo} alt="logo" />
     </Link>
     <nav>
       <NavLink
         exact="true"
-        activeclassname="active"
         className="home-link"
+        activeclassname="active"
         to="/"
       >
         <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
       </NavLink>
-      <NavLink exact="true" className="resume-link" activeclassname="active" to="/resume" >
+      <NavLink
+        exact="true"
+        className="resume-link"
+        activeclassname="active"
+        to="/resume"
+      >
         <FontAwesomeIcon icon={faFile} color="#4d4d4e" />
       </NavLink>
-      <NavLink exact="true" className="projects-link" activeclassname="active" to="/projects" >
+      <NavLink
+        exact="true"
+        className="projects-link"
+        activeclassname="active"
+        to="/projects"
+      >
         <FontAwesomeIcon icon={faProjectDiagram} color="#4d4d4e" />
       </NavLink>
-      <NavLink exact="true" className="blog-link" activeclassname="active" to="/blog">
+      <NavLink
+        exact="true"
+        className="blog-link"
+        activeclassname="active"
+        to="/blog"
+      >
         <FontAwesomeIcon icon={faBlog} color="#4d4d4e" />
       </NavLink>
-      <NavLink exact="true" className="contact-link" activeclassname="active" to="/contact" >
+      <NavLink
+        exact="true"
+        className="contact-link"
+        activeclassname="active"
+        to="/contact"
+      >
         <FontAwesomeIcon icon={faPhone} color="#4d4d4e" />
       </NavLink>
-      <NavLink exact="true" className="about-link" activeclassname="active" to="/about">
+      <NavLink
+        exact="true"
+        className="about-link"
+        activeclassname="active"
+        to="/about"
+      >
         <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
       </NavLink>
     </nav>
     <ul>
       <li>
-        <a target="_blank"
+        <a
+          target="_blank"
           rel="noreferrer"
           href="https://www.linkedin.com/in/kadencarr/"
-          >
+        >
           <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-          </a>
+        </a>
       </li>
       <li>
-        <a target="_blank"
+        <a
+          target="_blank"
           rel="noreferrer"
           href="https://github.com/Kccarr6022"
-          >
+        >
           <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-          </a>
+        </a>
       </li>
     </ul>
   </div>
